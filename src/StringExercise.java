ec122751d0e12a80b6bb0e3fe7777623dc08f6e0
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-20 12:30:47 -0600

    java exercises strings

---
+++ src/StringExercise.java
@@ -0,0 +1,6 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = "We don't need no education \nWe don't need no thought control";
+        System.out.println(message);
+    }
+}

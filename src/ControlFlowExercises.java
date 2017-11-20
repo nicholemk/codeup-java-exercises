634092db5b79ce49587886bbcaa4b499acf18566
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-20 11:56:26 -0600

    java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -115,18 +115,30 @@ import java.util.Scanner;
 //    }
 
 
-    public class ControlFlowExercises {
-        public static void main(String[] args) {
-            for (int i = 1; i <= 100; i += 1) {
-                if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.println("FizzBuzz");
-                } else if (i % 3 == 0) {
-                    System.out.println("Fizz");
-                } else if (i % 5 == 0) {
-                    System.out.println("Buzz");
-                    } else {
-                        System.out.println(i);
-                    }
-                }
-            }
-        }
+//    public class ControlFlowExercises {
+//        public static void main(String[] args) {
+//            for (int i = 1; i <= 100; i += 1) {
+//                if (i % 3 == 0 && i % 5 == 0) {
+//                    System.out.println("FizzBuzz");
+//                } else if (i % 3 == 0) {
+//                    System.out.println("Fizz");
+//                } else if (i % 5 == 0) {
+//                    System.out.println("Buzz");
+//                    } else {
+//                        System.out.println(i);
+//                    }
+//                }
+//            }
+//        }
+
+import java.util.Scanner;
+
+
+public class ControlFlowExercises{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What number would you like to go up to?");
+        int userInt = sc.nextInt();
+        System.out.println(userInt);
+    }
+}

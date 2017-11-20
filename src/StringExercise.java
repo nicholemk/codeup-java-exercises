8d873b8112976f33b6a05ed14a399553a5533ef7
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-20 16:24:41 -0600

    java exercises strings

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StringExercise {
     public static void main(String[] args) {
 //        String message = "We don't need no education \nWe don't need no thought control";
@@ -6,20 +8,31 @@ public class StringExercise {
 //        String message = "Check \"this\" out, \"s inside of \"s!";
 //        System.out.println(message);
 
-//        String message = "In windows, the main drive is usually C:/";
+//        String message = "In windows, the main drive is usually C:\\";
 //        System.out.println(message);
 
 //        String message = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\";
 //        System.out.println(message);
 
-        if (question) {
-            System.out.println("Sure.");
-        } else if (yelling) {
-            System.out.println("Whoa, chill out!");
-        } else if (nothing) {
-            System.out.println("Fine! Be that way.");
-        } else (anythingElse) {
-            System.out.println("Whatever.");
+//        if (question) {
+//            System.out.println("Sure.");
+//        } else if (yelling) {
+//            System.out.println("Whoa, chill out!");
+//        } else if (nothing) {
+//            System.out.println("Fine! Be that way.");
+//        } else (anythingElse) {
+//            System.out.println("Whatever.");
+//        }
+
+        System.out.println("Say something to Bob!");
+        Scanner scan = new Scanner(System.in);
+
+        String userInput = scan.nextLine();
+
+        //bob answers sure if you ask him a question
+
+        if(userInput.endsWith("?")) {
+
         }
     }
 }

d74499f20c9a37e7babf66f0e8e8b1d62717c22a
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-27 12:21:17 -0600

    added restaurant dish

--- src/RollDiceGame.java
+++ src/RollDiceGame.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class RollDiceGame {
+
+    // Ask the user to enter the number of sides for a pair of dice.
+    // Prompt the user to roll the dice.
+    //"Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+    //Use static methods to implement the method(s) that generate the random numbers.
+    //Use the .random method of the java.lang.Math class to generate random numbers.
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         rollDiceGame(sc);
+    }
+
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            System.out.println("Not a number!");
+            return getInteger(min, max);
+        }
+        int userInput = sc.nextInt();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else {
+            System.out.println("Number not in range!");
+            return getInteger(min, max);
+        }
+    }
 
         public static int randomWithRange(int min, int max)
         {
@@ -18,9 +44,8 @@ public class RollDiceGame {
                 do {
                     System.out.println("Would you like to roll again? [y/n]");
                     userChoice = sc.next().trim();
-                } while (!userChoice.equalsIgnoreCase("y") & !userChoice.equalsIgnoreCase("n"));
+                } while (!userChoice.equalsIgnoreCase("y") && !userChoice.equalsIgnoreCase("n"));
 
             } while (!userChoice.equals("n"));
         }
     }
-}
---
+++ src/restaurant.java
@@ -0,0 +1,17 @@
+public class restaurant {
+    public static void main(String[] args) {
+        RestaurantDish = new RestaurantDish();
+        dish.costInCents = 1000;
+        dish.nameOfDish = "Dumplings";
+        dish.wouldRecommend = true;
+        dish.eat();
+    }
+
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+    public void eat(); {
+        System.out.println("Nom");
+
+    }
+}

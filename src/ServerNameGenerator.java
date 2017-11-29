97e7dbd7367611b2b8ad0406390735fe915f1e2a
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-29 10:54:34 -0600

    finished movie exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,17 +1,17 @@
-public class ServerNameGenerator {
-    public static void main(String[] args) {
-
-        String[] nouns = {"dog", "cat", "bird", "lizard", "otter"};
-
-        String[] adjectives = {"funny", "cute", "weird", "ugly", "soft"};
-
-        String serverName = getRandom(adjectives) + " " + getRandom(nouns);
-
-        System.out.println("Your server name is: " + serverName);
-        }
-
-        public static String getRandom(String[] strings) {
-        int randomIndex = (int) (Math.random() * strings.length);
-        return strings[randomIndex];
-        }
-    }
+//public class ServerNameGenerator {
+//    public static void main(String[] args) {
+//
+//        String[] nouns = {"dog", "cat", "bird", "lizard", "otter"};
+//
+//        String[] adjectives = {"funny", "cute", "weird", "ugly", "soft"};
+//
+//        String serverName = getRandom(adjectives) + " " + getRandom(nouns);
+//
+//        System.out.println("Your server name is: " + serverName);
+//        }
+//
+//        public static String getRandom(String[] strings) {
+//        int randomIndex = (int) (Math.random() * strings.length);
+//        return strings[randomIndex];
+//        }
+//    }

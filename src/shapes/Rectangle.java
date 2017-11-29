7473d3eafc1fa71e99491659c9d71d185c6d6a67
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-29 12:35:02 -0600

    finished rectangle exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,20 @@
 package shapes;
 
 public class Rectangle {
+
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double perimeter() {
+        return this.length * 2 + this.width * 2;
+    }
+
+    public double area() {
+        return this.length * this.width;
+    }
 }

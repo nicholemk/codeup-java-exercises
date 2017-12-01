efe182a5644202e3bac8404918478a952051f715
nicholekirlin <nichole.kirlin@gmail.com>
2017-12-01 09:58:09 -0600

    student exercise

---
+++ src/Grades/Student.java
@@ -0,0 +1,19 @@
+package Grades;
+
+public static void main(String[]args){
+
+
+public class Student {
+    private final String name;
+    private int grades;
+
+    public Student(int grades) {
+        this.grades = grades;
+    }
+
+    private Student(String name) {
+        this.name = name;
+
+    }
+}
+        }
